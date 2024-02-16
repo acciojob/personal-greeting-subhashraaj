6c3910c964dd4a1c708f9b1603b3c3ce28b4b411
Subhash Raaj K <150801940+subhashraaj@users.noreply.github.com>
2024-02-16 21:08:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,18 @@ import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [inputValue, setInputValue] = userState("");
+    const [inputValue, setInputValue] = useState("");
     const onChangeText = (event) => {
         setInputValue(event.target.value);
-  }
-  return (
-    <div>
-        <lable htmlFor="name" >Enter your name<label>
-        <input id="name" onChnage={onChangeText} />
-        {inputValue && <p>{`Hello ${inputValue}!`}</p>}
-    </div>
-  )
+    }
+
+    return (
+        <div>
+            <label>Enter your name:</label>
+            <input onChange={onChangeText}></input>
+            {inputValue && <p>{`Hello ${inputValue}!`}</p>}
+        </div>
+    )
 }
 
 export default App
